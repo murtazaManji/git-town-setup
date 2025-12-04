5de9f9ff838b5c89eb905fa05869239e90a0a233
murtazaManjni <murtaza.m@keytoz.com>
2025-12-04 09:56:09 +0530

    feat(core): add new testing function

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ const test = () => {
   console.log("Hello, World! test git town changes here also we are here");
 };
 
+const testing = () => {
+  console.log("This is a new testing function after changes.");
+};
+
 test();
