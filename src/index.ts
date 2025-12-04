38a5f418bf498173ab478fd04da8a6be29fda93a
murtazaManji <167285573+murtazaManji@users.noreply.github.com>
2025-12-04 10:32:30 +0530

    Feature/1
    * feat(init): add initial TypeScript entry point with a test function
    
    * chore(index): update console log message
    
    * fix(logs): update console message in test function
    
    * chore(logging): update console message in test function
    
    * feat(core): add new testing function

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const test = () => {
+  console.log("Hello, World! test git town changes here also we are here");
+};
+
+const testing = () => {
+  console.log("This is a new testing function after changes.");
+};
+
+test();
