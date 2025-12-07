a23798c636610de347761e39412452262bf81160
murtazaManji <167285573+murtazaManji@users.noreply.github.com>
2025-12-07 19:59:55 +0530

    Other fetaure 1
    * feat(logging): add initial logging function in Hii.ts
    
    * feat(core): add test1234 function with console log for Git operation testing
    
    * fix(Hii): correct typo in console log message
    
    * feat(core): add new function test1235 with console log
    
    * chore(src): update console log message in Hii copy.ts
    
    * refactor(logging): update greeting message in test1234 function

---
+++ src/Hii copy.ts
@@ -0,0 +1,11 @@
+const test1234 = () => {
+  console.log(
+    "Hello, World! test git town changes here also we are here this is the one change we are here also he hello"
+  );
+};
+
+const test1235 = () => {
+  console.log(
+    "Hello, World! test git town changes here also we are here this is the one change we are here also"
+  );
+};
